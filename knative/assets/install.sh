aaf3c9f2e0efb64e9f4a60ebae3e9d8d0ed21229
Alexander Krieg <alitari67@gmail.com>
2020-05-28 15:51:57 +0000

    Remove steps

--- knative/assets/install.sh
+++ knative/assets/install.sh
@@ -1,2 +1,3 @@
-#!/usr/bin/bash
-kubectl apply --filename https://github.com/knative/serving/releases/download/v0.13.0/serving-crds.yaml
+kubeadm init --kubernetes-version $(kubeadm version -o short)
+
+# kubectl apply --filename https://github.com/knative/serving/releases/download/v0.13.0/serving-crds.yaml
