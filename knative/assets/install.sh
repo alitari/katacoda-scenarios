2ff7ae4a83bba0e865235ee55ba2acd58b3401ec
Alexander Krieg <alitari67@gmail.com>
2020-05-28 15:59:40 +0000

    Add shebang

--- knative/assets/install.sh
+++ knative/assets/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 kubeadm init --kubernetes-version $(kubeadm version -o short)
 
 # kubectl apply --filename https://github.com/knative/serving/releases/download/v0.13.0/serving-crds.yaml
