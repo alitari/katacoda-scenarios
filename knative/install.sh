281ec1911ecef109f942a0e3f0281c318ac62cea
Alexander Krieg <alitari67@gmail.com>
2020-05-28 16:29:29 +0000

    Copy config

--- knative/install.sh
+++ knative/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-kubeadm init --kubernetes-version $(kubeadm version -o short)
+# kubeadm init --kubernetes-version $(kubeadm version -o short)
+mkdir ~/.kube
+cp  /etc/kubernetes/admin.conf ~/.kube/config
 
 # kubectl apply --filename https://github.com/knative/serving/releases/download/v0.13.0/serving-crds.yaml
